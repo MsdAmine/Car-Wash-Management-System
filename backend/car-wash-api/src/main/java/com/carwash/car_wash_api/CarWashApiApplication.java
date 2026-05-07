fc07dda5fe6b5f9d7aa6de2b0cabca1a50798e8e
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2026-05-07 02:21:35 +0200

    Create Spring Boot backend project

---
+++ backend/car-wash-api/src/main/java/com/carwash/car_wash_api/CarWashApiApplication.java
@@ -0,0 +1,12 @@
+package com.carwash.car_wash_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWashApiApplication.class, args);
+    }
+}
